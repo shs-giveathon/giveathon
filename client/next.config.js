39e137735b52469633790ffe96129145ade224ad
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-09-27 16:43:37 -0500

    add scss

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const path = require('path');
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  }
+};
+
+module.exports = nextConfig;
