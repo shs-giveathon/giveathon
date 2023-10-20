f4ab2a746d8b46da3a97b629166a45935b18b0d2
andrew <me@andreww.co>
2023-10-19 19:54:54 -0500

    add font header for profile

--- client/app/[profile]/[profileId]/page.tsx
+++ client/app/[profile]/[profileId]/page.tsx
@@ -32,14 +32,14 @@ const ProfileId: NextPage = () => {
     <>
       <Navbar />
       <div className='w-full h-screen pt-16'>
-        <div className='text-center font-bold text-3xl mt-4'>Give-A-Thon Profile</div>
-        <div className='grid place-items-center gap-2'>
+      <div className='fade-in glass-effect rounded-lg w-1/2 mx-auto font-headers text-center font-bold mt-4 text-3xl xs:text-4xl lg:text-6xl'>GiveAThn Profile</div>{' '}
+        <div className='mt-4 grid place-items-center gap-2'>
           <div className='w-[calc(100%-20px)] md:w-[80%] glass-effect text-center rounded-md p-2 fade-in'>
             <p>{decodeURIComponent(params.profileId as string)}</p>
           </div>
           {data ? (
             <div className='w-[calc(100%-20px)] md:w-[80%] glass-effect text-center rounded-md p-2 fade-in'>
-              <p className='font-semibold'>{data.Affiliation}</p>
+              <p className='font-semibold text-xl'>{data.Affiliation}</p>
               <p>History</p>
               <div>
                 {data.History.map((h: any) => {
