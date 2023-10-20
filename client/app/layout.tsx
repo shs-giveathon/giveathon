376c9a46042a2d443bfea85152a0d0e57e8de392
andrew <me@andreww.co>
2023-10-19 20:05:55 -0500

    add hi-res favicon

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -4,6 +4,9 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'GiveAThon 2023 🕷️',
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎁</text></svg>"
+  }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
