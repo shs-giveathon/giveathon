15c0404c348d5b36728a5e5b2ee988c1f79736ba
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-20 20:23:22 -0500

    Update page.tsx

--- client/app/affiliation/[affiliationId]/page.tsx
+++ client/app/affiliation/[affiliationId]/page.tsx
@@ -38,7 +38,7 @@ const AffiliationIdPage: NextPage = () => {
     <>
       <Navbar />
       <div className='w-full h-screen pt-16'>
-        <div className='fade-in glass-effect rounded-lg w-1/2 mx-auto font-headers text-center font-bold mt-4 text-3xl xs:text-4xl lg:text-6xl'>GiveAThn Profile</div>{' '}
+        <div className='fade-in glass-effect rounded-lg w-1/2 mx-auto font-headers text-center font-bold mt-4 text-3xl xs:text-4xl lg:text-6xl'>GiveAThn Affiliation</div>{' '}
         <div className='mt-4 grid place-items-center gap-2'>
           <div className='w-[calc(100%-20px)] md:w-[80%] glass-effect text-center rounded-md p-2 fade-in'>
             <p>{decodeURIComponent(params.affiliationId as string)}</p>
