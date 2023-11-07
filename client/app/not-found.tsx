72749c9758ce2212cd3fcd9a427683a45a9693d1
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-06 20:00:19 -0600

    Update not-found.tsx

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -1,3 +1,4 @@
+import { Home } from 'lucide-react';
 import { NextPage } from 'next';
 import Link from 'next/link';
 
@@ -6,9 +7,14 @@ const NotFoundPage: NextPage = () => {
     <div className='h-screen w-full'>
       <div className='w-max mx-auto grid place-items-center h-full'>
         <div className='glass-effect fade-in py-4 px-14 text-center rounded-md'>
-          <p>page not found :(</p>
+          <p className='font-semibold'>page not found :(</p>
           <Link href='/'>
-            <p className='hover:text-blue-500 transition'>return home?</p>
+            <button className='mt-2 px-2 py-1 rounded-md relative no-underline bg-blue-500 hover:bg-blue-600 transition'>
+              <div className='grid grid-cols-[auto_max-content] gap-1 place-items-center'>
+                <Home className='w-4 h-4 text-white' />
+                <span className='text-white'>Return home</span>
+              </div>
+            </button>
           </Link>
         </div>
       </div>
